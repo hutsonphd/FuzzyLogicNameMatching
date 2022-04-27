ea95840dd13b27a62372d4e18e837b394ea1fdd9
Dr. Hutson <hutsona@me.com>
2022-04-27 18:40:41 -0500

    containerized

--- WattTime/entrypoint.sh
+++ WattTime/entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate
 python manage.py collectstatic --no-input
 
-gunicorn Aureum.wsgi:application --bind 0.0.0.0:8000 --timeout 300 --workers=3
+gunicorn WattTime.wsgi:application --bind 0.0.0.0:8000 --timeout 300 --workers=3
